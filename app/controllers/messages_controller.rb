fc129a0b9c09058e81792618a256dc4ea45091f2
yuka <tyty10072019@gmail.com>
2021-06-24 22:43:06 +0900

    new/create作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,16 @@
 class MessagesController < ApplicationController
   
+  def new
+    @message = Message.new
+  end
+
+  def create
+    Message.create(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:message.permit(:name, :contents)
+  end
+
 end
