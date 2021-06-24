067fa76957a1cfb25972e0ac1ecfe5fb7ed9c39e
yuka <tyty10072019@gmail.com>
2021-06-24 22:11:13 +0900

    DBマイグレート

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_20_132439) do
+ActiveRecord::Schema.define(version: 2021_06_24_130621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string "name"
+    t.string "contents"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
