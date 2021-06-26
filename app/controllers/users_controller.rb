e385b2f7f784123fda1eb300c305f8d9888df610
kindness89 <yuusan29blog@gmail.com>
2021-06-27 00:17:57 +0900

    index, show, edit, update, destroy実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,32 @@
 class UsersController < ApplicationController
-
   def index
     @users = User.all
   end
 
   def new
-    @user = Usre.new
+    @user = User.new
   end
 
   def create
     User.create(user_params)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
   end
 
     private
