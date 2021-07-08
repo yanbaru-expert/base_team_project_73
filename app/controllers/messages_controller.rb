f7c8c922562c6b3db647f236a504a7d5ba63dd2b
yuka <tyty10072019@gmail.com>
2021-07-08 23:34:20 +0900

    show edit update destroyを追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,37 @@
 class MessagesController < ApplicationController
-  
+  def index
+    @messages = Message.all
+  end
+
   def new
     @message = Message.new
   end
 
   def create
-    Message.create(user_params)
+    Message.create(message_params)
+  end
+
+  def edit
+    @message = Message.find(params[:id])
+  end
+
+  def update
+    message = Message.find(params[:id])
+    message.update(message_params)
+  end
+
+  def show
+    @message = Message.find(params[:id])
+  end
+
+  def destroy
+    message = Message.find(params[:id])
+    message.destroy
   end
 
   private
   def user_params
-    params.require(:message.permit(:name, :contents)
+    params.require(:message).permit(:name, :contents)
   end
 
 end
